bb279742827b31ea25b2e51acfed84ac05a2bc97
jsepucas <jsepucas@myuax.com>
2023-11-15 19:16:23 +0100

    4.5

--- Ejercicio 4/Subespacios.cpp
+++ Ejercicio 4/Subespacios.cpp
@@ -10,12 +10,19 @@ namespace Ciencia {
             return masa * velocidadLuz * velocidadLuz;
         }
     }
-}
+    namespace Quimica {
+        const double numeroAvogadro = 6.02214076e23;
+
+    }
+    namespace Biologia {
+            double calcularIMC(double peso, double altura) {
+                return peso / (altura * altura);
+            }
+ }
 int main() {
     double masa;
     std::cout << "Introduce la masa en kilogramos: ";
     std::cin >> masa;
-
     double energia = Ciencia::Fisica::energia(masa);
 
     std::cout << "La energía equivalente es: " << energia << " julios" << std::endl;
