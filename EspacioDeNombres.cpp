b35bf855d771acf3ee19cb89b206daf6707bf9f3
jsepucas <jsepucas@myuax.com>
2023-11-15 17:28:23 +0100

    1.7

--- EspacioDeNombres.cpp
+++ EspacioDeNombres.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 namespace matematicas {
     int suma(int a, int b) {
@@ -8,11 +10,15 @@ namespace matematicas {
     int resta(int a, int b) {
         return a - b;
     }
+
+    int multiplicacion(int a, int b) {
+        return a * b;
+    }
 }
 
 int main() {
     int numero1 = 20;
-    int numero2 = 5;
+    int numero2 = 9;
 
     int solucion_suma = matematicas::suma(numero1, numero2);
     int solucion_resta = matematicas::resta(numero1, numero2);
