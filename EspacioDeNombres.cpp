0dbea5066018aac52312d440b665d5388af1be04
jsepucas <jsepucas@myuax.com>
2023-11-15 17:32:08 +0100

    1.8

--- EspacioDeNombres.cpp
+++ EspacioDeNombres.cpp
@@ -14,6 +14,16 @@ namespace matematicas {
     int multiplicacion(int a, int b) {
         return a * b;
     }
+
+    double division(double a, double b) {
+        if(b ! = 0) {
+            return a / b;
+        } else {
+            cout << "No se puede dividir entre 0" << endl;
+            return 0;
+        }
+
+    }
 }
 
 int main() {
