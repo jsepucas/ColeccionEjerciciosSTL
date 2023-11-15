dd7e4b107f76dcc60975d6d13bfadb005a357c9f
jsepucas <jsepucas@myuax.com>
2023-11-15 18:51:57 +0100

    3.5

--- Ejercicio 3/main2.cpp
+++ Ejercicio 3/main2.cpp
@@ -2,3 +2,13 @@
 #include "circunferencia.cpp"
 #include "decagono.cpp"
 #include "heptagono.cpp"
+
+using namespace std;
+
+int main() {
+    Geometria::Circulo c(5);
+    cout << "Area del circulo: " << c.calcularArea() << endl;
+    cout << "Perimetro del circulo: " << c.calcularPerimetro() << endl;
+
+
+}
