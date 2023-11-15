3d2ac22b42985f7bd0de568677124b8dab18e415
jsepucas <jsepucas@myuax.com>
2023-11-15 18:45:18 +0100

    3.3

--- Ejercicio 3/heptagono.cpp
+++ Ejercicio 3/heptagono.cpp
@@ -7,3 +7,10 @@ namespace Geometria {
     public:
         Heptagono(double l) : lado(l) {}
 
+        double calcularArea() {
+            double apotema = lado / (2 * tan(M_PI / 7));
+            double perimetro = 7 * lado;
+            return (perimetro * apotema) / 2;
+        }
+
+
