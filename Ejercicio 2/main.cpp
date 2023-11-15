53473459e3620dd4c09b23557f1e2b9c0a90ce18
jsepucas <jsepucas@myuax.com>
2023-11-15 18:29:58 +0100

    2.7

--- Ejercicio 2/main.cpp
+++ Ejercicio 2/main.cpp
@@ -4,3 +4,19 @@
 #include "hexagono.cpp"
 #include "cuadrado.cpp"
 #include "rectangulo.cpp"
+
+int main() {
+    double radioCirculo = 6;
+    double baseTriangulo = 4;
+    double alturaTriangulo = 5;
+    double ladoHexagono = 8;
+    double ladoCuadrado = 7;
+    double largoRectangulo = 10;
+    double anchoRectangulo = 5;
+
+    double areaCirculo = Geometria::Area_circulo(radioCirculo);
+    double areaTriangulo = Geometria::Area_triangulo(baseTriangulo, alturaTriangulo);
+    double areaHexagono = Geometria::Area_hexagono(ladoHexagono);
+    double areaCuadrado = Geometria::Area_cuadrado(ladoCuadrado);
+    double areaRectangulo = Geometria::Area_rectangulo(largoRectangulo, anchoRectangulo);
+}
