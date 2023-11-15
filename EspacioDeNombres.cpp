0c59fa694d71d45d145163cb10cf7ba670059d3c
jsepucas <jsepucas@myuax.com>
2023-11-15 17:22:56 +0100

    1.4

--- EspacioDeNombres.cpp
+++ EspacioDeNombres.cpp
@@ -13,4 +13,7 @@ namespace matematicas {
 int main() {
     int numero1 = 20;
     int numero2 = 5;
+
+    int solucion_suma = matematicas::suma(numero1, numero2);
+    int solucion_resta = matematicas::resta(numero1, numero2);
 }
